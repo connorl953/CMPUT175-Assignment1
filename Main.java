36eb115f990bae7c6c0043aa1ce8559977c05c37
connorl953 <connor009988@gmail.com>
2023-09-16 14:55:04 -0600

    Deleted java files

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args){
-    int number = 0;
-    while (number < 100000000){
-        number++;
-        System.out.println(number);
-    }
-  }
-}
